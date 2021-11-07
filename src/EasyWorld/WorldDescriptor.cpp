ac2971c5d4fb479b9767e27dfded2ca2f10ec203
chLi <565353780@qq.com>
2021-11-08 00:47:06 +0800

    update data class

--- src/EasyWorld/WorldDescriptor.cpp
+++ src/EasyWorld/WorldDescriptor.cpp
@@ -1,52 +1,516 @@
 #include "WorldDescriptor.h"
 
-bool RoomProp::reset()
+bool WorldData::reset()
 {
-    name = "Name Undefined";
+    world_center_x = 0;
+    world_center_y = 0;
+
+    return true;
+}
+
+bool WorldData::outputInfo(
+    const size_t &info_level)
+{
+    std::string line_start = "";
+    for(size_t i = 0; i < info_level; ++i)
+    {
+        line_start += "\t";
+    }
+
+    std::cout << "WorldData : " << std::endl <<
+      "\tworld_center = [" << world_center_x << "," <<
+      world_center_y << "]" << std::endl;
+
+    return true;
+}
+
+bool WallData::reset()
+{
+    id = 0;
+    type = NodeType::NodeFree;
+    boundary_polygon.reset();
+
+    return true;
+}
+
+bool WallData::outputInfo(
+    const size_t &info_level)
+{
+    std::string line_start = "";
+    for(size_t i = 0; i < info_level; ++i)
+    {
+        line_start += "\t";
+    }
+
+    std::cout << line_start << "WallData : " << std::endl <<
+      line_start << "\tid = " << id << std::endl <<
+      line_start << "\ttype = " << type << std::endl;
+
+    return true;
+}
+
+bool WallRoomContainerData::reset()
+{
+    wall_id = 0;
+    wall_type = NodeType::NodeFree;
+    on_wall_boundary_idx = 0;
+    on_wall_boundary_start_position = -1;
     target_width = -1;
     real_width = -1;
     target_height = -1;
     real_height = -1;
 
-    roomcontainer_id_ = 0;
-    room_id_ = 0;
+    room_num = 0;
+    room_name_vec.clear();
 
     return true;
 }
 
-bool RoomProp::setRoomProp(
-    const std::string &room_name,
-    const float &room_target_width,
-    const float &room_target_height)
+bool WallRoomContainerData::outputInfo(
+    const size_t &info_level)
 {
-    reset();
-
-    if(room_target_width <= 0 || room_target_height <= 0)
+    std::string line_start = "";
+    for(size_t i = 0; i < info_level; ++i)
     {
-        std::cout << "RoomProp::setRoomProp : " << std::endl <<
-          "Input :\n" <<
-          "\troom_name = " << room_name << std::endl <<
-          "\troom_target_size = [" << room_target_width << "," <<
-          room_target_height << "]" << std::endl <<
-          "room target size not valid!" << std::endl;
-
-        return false;
+        line_start += "\t";
     }
 
-    name = room_name;
-    target_width = room_target_width;
-    target_height = room_target_height;
+    std::cout << line_start << "WallRoomContainerData : " << std::endl <<
+      line_start << "\twall_id = " << wall_id << std::endl <<
+      line_start << "\twall_type = " << wall_type << std::endl <<
+      line_start << "\ton_wall_boundary_idx = " << on_wall_boundary_idx << std::endl <<
+      line_start << "\ton_wall_boundary_start_position = " << on_wall_boundary_start_position << std::endl <<
+      line_start << "\ttarget_size = [" << target_width << "," << target_height << "]" << std::endl <<
+      line_start << "\treal_size = [" << real_width << "," << real_height << "]" << std::endl <<
+      line_start << "\troom_num = " << room_num << std::endl <<
+      line_start << "\troom_name_vec = ";
+
+    if(room_name_vec.size() == 0)
+    {
+        std::cout << "NULL" << std::endl;
+    }
+    else
+    {
+        for(const std::string &room_name : room_name_vec)
+        {
+            std::cout << room_name << " ";
+        }
+
+        std::cout << std::endl;
+    }
 
     return true;
 }
 
-bool RoomContainerProp::reset()
+bool FreeRoomContainerData::reset()
 {
+    team_x_direction_person_num = 0;
+    team_y_direction_person_num = 0;
+    team_dist = -1;
+    person_edge = -1;
+
+    return true;
+}
+
+bool FreeRoomContainerData::outputInfo(
+    const size_t &info_level)
+{
+    std::string line_start = "";
+    for(size_t i = 0; i < info_level; ++i)
+    {
+        line_start += "\t";
+    }
+
+    std::cout << line_start << "FreeRoomContainerData : " << std::endl <<
+      line_start << "\tteam_x_direction_person_num = " << team_x_direction_person_num << std::endl <<
+      line_start << "\tteam_y_direction_person_num = " << team_y_direction_person_num << std::endl <<
+      line_start << "\tteam_dist = " << team_dist << std::endl <<
+      line_start << "\tperson_edge = " << person_edge << std::endl;
+
     return true;
 }
 
 bool WorldDescriptor::reset()
 {
+    world_data.reset();
+    wall_data_vec.clear();
+    wall_roomcontainer_data_vec.clear();
+    free_roomcontainer_data.reset();
+
+    return true;
+}
+
+bool WorldDescriptor::readData(
+    WorldController &world_controller,
+    WorldPlaceGenerator &world_place_generator)
+{
+    if(!reset())
+    {
+        std::cout << "WorldDescriptor::readData : " << std::endl <<
+          "reset world generate data manager failed!" << std::endl;
+
+        return false;
+    }
+
+    EasyNode* world_node = world_controller.findNode(
+        0, NodeType::World);
+
+    if(world_node == nullptr)
+    {
+        std::cout << "WorldDescriptor::readData : " << std::endl <<
+          "world node not found!" << std::endl;
+
+        return false;
+    }
+
+    const EasyAxis2D &world_axis_in_world =
+      world_node->getAxisInWorld();
+
+    if(!setWorld(
+          world_node->getName(),
+          world_axis_in_world.center_.x,
+          world_axis_in_world.center_.y))
+    {
+        std::cout << "WorldDescriptor::readData : " << std::endl <<
+          "setWorldCenter failed!" << std::endl;
+
+        return false;
+    }
+
+    for(EasyNode* wall_node : world_node->getChildNodeVec())
+    {
+        const NodeType &wall_type = wall_node->getNodeType();
+
+        if(wall_type != NodeType::OuterWall &&
+            wall_type != NodeType::InnerWall)
+        {
+            continue;
+        }
+
+        EasyNode* wall_space_node = wall_node->findChild(0, NodeType::Space);
+
+        if(wall_space_node == nullptr)
+        {
+            std::cout << "WorldDescriptor::readData : " << std::endl <<
+              "wall space node is nullptr!" << std::endl;
+
+            return false;
+        }
+
+        if(!addWall(
+              wall_node->getName(),
+              wall_node->getID(),
+              wall_node->getNodeType(),
+              wall_space_node->getBoundaryPolygon()))
+        {
+            std::cout << "WorldDescriptor::readData : " << std::endl <<
+              "addWall failed!" << std::endl;
+
+            return false;
+        }
+    }
+
+    const size_t wall_roomcontainer_data_num =
+      world_place_generator.boundary_line_list_manager_.current_place_idx_;
+    
+    if(wall_roomcontainer_data_num == 0)
+    {
+        return true;
+    }
+
+    wall_roomcontainer_data_vec.resize(
+        wall_roomcontainer_data_num);
+
+    for(size_t i = 0;
+        i < world_place_generator.boundary_line_list_manager_.boundary_line_list_vec_.size();
+        ++i)
+    {
+        const BoundaryLineList &boundary_line_list =
+          world_place_generator.boundary_line_list_manager_.boundary_line_list_vec_[i];
+
+        BoundaryLine* boundary_line = boundary_line_list.boundary_line_list_;
+
+        while(boundary_line != nullptr)
+        {
+            if(!setWallRoomContainer(
+                  boundary_line->place_idx,
+                  0,
+                  NodeType::OuterWall,
+                  i,
+                  boundary_line->line_start_position,
+                  boundary_line->line_width,
+                  boundary_line->line_end_position - boundary_line->line_start_position,
+                  boundary_line->line_height,
+                  boundary_line->line_real_height,
+                  boundary_line->line_room_num,
+                  boundary_line->room_name_vec))
+            {
+                std::cout << "WorldDescriptor::readData : " << std::endl <<
+                  "setWallRoomContainer failed!" << std::endl;
+
+                return false;
+            }
+
+            boundary_line = boundary_line->next_line;
+        }
+    }
+
+    if(!setFreeRoomContainer(
+          world_place_generator.team_x_direction_person_num_,
+          world_place_generator.team_y_direction_person_num_,
+          world_place_generator.team_dist_,
+          world_place_generator.person_edge_))
+    {
+        std::cout << "WorldDescriptor::readData : " << std::endl <<
+          "setFreeRoomContainer failed!" << std::endl;
+
+        return false;
+    }
+
+    return true;
+}
+
+bool WorldDescriptor::loadData(
+    WorldController &world_controller,
+    WorldPlaceGenerator &world_place_generator)
+{
+    const float &world_center_x = world_data.world_center_x;
+    const float &world_center_y = world_data.world_center_y;
+
+    if(!world_place_generator.reset())
+    {
+        std::cout << "WorldDescriptor::loadData : " << std::endl <<
+          "reset world place generator failed!" << std::endl;
+
+        return false;
+    }
+
+    if(!world_place_generator.createNewWorld(
+          world_controller,
+          world_center_x,
+          world_center_y))
+    {
+        std::cout << "WorldDescriptor::loadData : " << std::endl <<
+          "createNewWorld failed!" << std::endl;
+
+        return false;
+    }
+
+    if(wall_data_vec.size() == 0)
+    {
+        std::cout << "WorldDescriptor::loadData : " << std::endl <<
+          "no wall data found!" << std::endl;
+
+        return false;
+    }
+
+    for(const WallData &wall_data : wall_data_vec)
+    {
+        if(wall_data.type == NodeType::OuterWall)
+        {
+            world_place_generator.outerwall_boundary_polygon_ = wall_data.boundary_polygon;
+            world_place_generator.is_outerwall_boundary_polygon_set_ = true;
+        }
+
+        if(!world_place_generator.generateWall(world_controller,
+              wall_data.name,
+              wall_data.type,
+              wall_data.boundary_polygon))
+        {
+            std::cout << "WorldDescriptor::loadData : " << std::endl <<
+              "generateWall failed!" << std::endl;
+
+            return false;
+        }
+    }
+
+    for(const WallRoomContainerData &wall_roomcontainer_data :
+        wall_roomcontainer_data_vec)
+    {
+        if(!world_place_generator.placeWallRoomContainer(
+              world_controller,
+              wall_roomcontainer_data.wall_id,
+              wall_roomcontainer_data.wall_type,
+              wall_roomcontainer_data.on_wall_boundary_idx,
+              wall_roomcontainer_data.on_wall_boundary_start_position,
+              wall_roomcontainer_data.target_width,
+              wall_roomcontainer_data.target_height,
+              wall_roomcontainer_data.room_num,
+              wall_roomcontainer_data.room_name_vec))
+        {
+            // std::cout << "WorldDescriptor::loadData : " << std::endl <<
+            //   "placeWallRoomContainer failed!" << std::endl;
+
+            continue;
+        }
+    }
+
+    world_place_generator.team_x_direction_person_num_ =
+      free_roomcontainer_data.team_x_direction_person_num;
+    world_place_generator.team_y_direction_person_num_ =
+      free_roomcontainer_data.team_y_direction_person_num;
+    world_place_generator.team_dist_ =
+      free_roomcontainer_data.team_dist;
+    world_place_generator.person_edge_ =
+      free_roomcontainer_data.person_edge;
+
+    if(!world_place_generator.generateFreeRoomContainer(
+          world_controller,
+          free_roomcontainer_data.team_x_direction_person_num,
+          free_roomcontainer_data.team_y_direction_person_num,
+          free_roomcontainer_data.team_dist,
+          free_roomcontainer_data.person_edge))
+    {
+        std::cout << "WorldDescriptor::loadData : " << std::endl <<
+          "generateFreeRoomContainer failed!" << std::endl;
+
+        return false;
+    }
+
+    return true;
+}
+
+bool WorldDescriptor::setWorld(
+    const std::string &name,
+    const float &world_center_x,
+    const float &world_center_y)
+{
+    world_data.name = name;
+    world_data.world_center_x = world_center_x;
+    world_data.world_center_y = world_center_y;
+
+    return true;
+}
+
+bool WorldDescriptor::addWall(
+    const std::string &name,
+    const size_t &id,
+    const NodeType &type,
+    const EasyPolygon2D &boundary_polygon)
+{
+    WallData new_wall_data;
+    new_wall_data.name = name;
+    new_wall_data.id = id;
+    new_wall_data.type = type;
+    new_wall_data.boundary_polygon = boundary_polygon;
+
+    wall_data_vec.emplace_back(new_wall_data);
+
+    return true;
+}
+
+bool WorldDescriptor::addWallRoomContainer(
+    const size_t &wall_id,
+    const NodeType &wall_type,
+    const size_t &on_wall_boundary_idx,
+    const float &on_wall_boundary_start_position,
+    const float &target_width,
+    const float &real_width,
+    const float &target_height,
+    const float &real_height,
+    const size_t &room_num,
+    const std::vector<std::string> &room_name_vec)
+{
+    WallRoomContainerData new_wall_roomcontainer_data;
+    new_wall_roomcontainer_data.wall_id = wall_id;
+    new_wall_roomcontainer_data.wall_type = wall_type;
+    new_wall_roomcontainer_data.on_wall_boundary_idx = on_wall_boundary_idx;
+    new_wall_roomcontainer_data.on_wall_boundary_start_position = on_wall_boundary_start_position;
+    new_wall_roomcontainer_data.target_width = target_width;
+    new_wall_roomcontainer_data.real_width = real_width;
+    new_wall_roomcontainer_data.target_height = target_height;
+    new_wall_roomcontainer_data.real_height = real_height;
+    new_wall_roomcontainer_data.room_num = room_num;
+    new_wall_roomcontainer_data.room_name_vec = room_name_vec;
+
+    wall_roomcontainer_data_vec.emplace_back(new_wall_roomcontainer_data);
+
+    return true;
+}
+
+bool WorldDescriptor::setWallRoomContainer(
+    const size_t &wall_roomcontainer_data_idx,
+    const size_t &wall_id,
+    const NodeType &wall_type,
+    const size_t &on_wall_boundary_idx,
+    const float &on_wall_boundary_start_position,
+    const float &target_width,
+    const float &real_width,
+    const float &target_height,
+    const float &real_height,
+    const size_t &room_num,
+    const std::vector<std::string> &room_name_vec)
+{
+    if(wall_roomcontainer_data_idx >= wall_roomcontainer_data_vec.size())
+    {
+        std::cout << "WorldDescriptor::setWallRoomContainer : " << std::endl <<
+          "Input :\n" <<
+          "\twall_roomcontainer_data_idx = " << wall_roomcontainer_data_idx << std::endl <<
+          "\twall_id = " << wall_id << std::endl <<
+          "\twall_type = " << wall_type << std::endl <<
+          "\ton_wall_boundary_idx = " << on_wall_boundary_idx << std::endl <<
+          "\ton_wall_boundary_start_position = " << on_wall_boundary_start_position << std::endl <<
+          "\ttarget_size = [" << target_width << "," << target_height << "]" << std::endl <<
+          "\treal_size = [" << real_width << "," << real_height << "]" << std::endl <<
+          "\troom_num = " << room_num << std::endl <<
+          "wall_roomcontainer_data_idx out of range!" << std::endl;
+
+        return false;
+    }
+
+    WallRoomContainerData &wall_roomcontainer_data = wall_roomcontainer_data_vec[wall_roomcontainer_data_idx];
+    wall_roomcontainer_data.wall_id = wall_id;
+    wall_roomcontainer_data.wall_type = wall_type;
+    wall_roomcontainer_data.on_wall_boundary_idx = on_wall_boundary_idx;
+    wall_roomcontainer_data.on_wall_boundary_start_position = on_wall_boundary_start_position;
+    wall_roomcontainer_data.target_width = target_width;
+    wall_roomcontainer_data.real_width = real_width;
+    wall_roomcontainer_data.target_height = target_height;
+    wall_roomcontainer_data.real_height = real_height;
+    wall_roomcontainer_data.room_num = room_num;
+    wall_roomcontainer_data.room_name_vec = room_name_vec;
+
+    return true;
+}
+
+bool WorldDescriptor::setFreeRoomContainer(
+    const size_t &team_x_direction_person_num,
+    const size_t &team_y_direction_person_num,
+    const float &team_dist,
+    const float &person_edge)
+{
+    free_roomcontainer_data.team_x_direction_person_num = team_x_direction_person_num;
+    free_roomcontainer_data.team_y_direction_person_num = team_y_direction_person_num;
+    free_roomcontainer_data.team_dist = team_dist;
+    free_roomcontainer_data.person_edge = person_edge;
+
+    return true;
+}
+
+bool WorldDescriptor::outputInfo(
+    const size_t &info_level)
+{
+    std::string line_start = "";
+    for(size_t i = 0; i < info_level; ++i)
+    {
+        line_start += "\t";
+    }
+
+    std::cout << line_start << "WorldDescriptor : " << std::endl;
+    world_data.outputInfo(info_level + 1);
+
+    for(WallData &wall_data : wall_data_vec)
+    {
+        wall_data.outputInfo(info_level + 1);
+    }
+
+    for(WallRoomContainerData &wall_roomcontainer_data : wall_roomcontainer_data_vec)
+    {
+        wall_roomcontainer_data.outputInfo(info_level + 1);
+    }
+
+    free_roomcontainer_data.outputInfo(info_level + 1);
+
     return true;
 }
 
--- src/EasyWorld/WorldDescriptor.h
+++ src/EasyWorld/WorldDescriptor.h
@@ -7,43 +7,84 @@
 #include "WorldController.h"
 #include "WorldPlaceGenerator.h"
 
-class RoomProp
+class WorldData
 {
 public:
-    RoomProp()
+    WorldData()
     {
     }
 
     bool reset();
+    
+    bool outputInfo(
+        const size_t &info_level);
 
-    bool setRoomProp(
-        const std::string &room_name,
-        const float &room_target_width,
-        const float &room_target_height);
+    float world_center_x;
+    float world_center_y;
 
     std::string name;
-    float target_width;
-    float target_height;
-
-    size_t roomcontainer_id_;
-    size_t room_id_;
-
-    float real_width;
-    float real_height;
 };
 
-class RoomContainerProp
+class WallData
 {
 public:
-    RoomContainerProp()
+    WallData()
     {
     }
 
     bool reset();
 
-    std::vector<RoomProp> room_prop_vec;
+    bool outputInfo(
+        const size_t &info_level);
 
-    size_t roomcontainer_id_;
+    size_t id;
+    NodeType type;
+    EasyPolygon2D boundary_polygon;
+
+    std::string name;
+};
+
+class WallRoomContainerData
+{
+public:
+    WallRoomContainerData()
+    {
+    }
+
+    bool reset();
+
+    bool outputInfo(
+        const size_t &info_level);
+
+    size_t wall_id;
+    NodeType wall_type;
+    size_t on_wall_boundary_idx;
+    float on_wall_boundary_start_position;
+    float target_width;
+    float real_width;
+    float target_height;
+    float real_height;
+
+    size_t room_num;
+    std::vector<std::string> room_name_vec;
+};
+
+class FreeRoomContainerData
+{
+public:
+    FreeRoomContainerData()
+    {
+    }
+
+    bool reset();
+
+    bool outputInfo(
+        const size_t &info_level);
+
+    size_t team_x_direction_person_num;
+    size_t team_y_direction_person_num;
+    float team_dist;
+    float person_edge;
 };
 
 class WorldDescriptor
@@ -55,10 +96,65 @@ public:
 
     bool reset();
 
-    bool addRoomContainer(
+    bool readData(
+        WorldController &world_controller,
+        WorldPlaceGenerator &world_place_generator);
+
+    bool loadData(
+        WorldController &world_controller,
+        WorldPlaceGenerator &world_place_generator);
+
+private:
+    bool setWorld(
+        const std::string &name,
+        const float &world_center_x,
+        const float &world_center_y);
+
+    bool addWall(
+        const std::string &name,
+        const size_t &id,
+        const NodeType &type,
+        const EasyPolygon2D &boundary_polygon);
+
+    bool addWallRoomContainer(
+        const size_t &wall_id,
+        const NodeType &wall_type,
+        const size_t &on_wall_boundary_idx,
+        const float &on_wall_boundary_start_position,
+        const float &target_width,
+        const float &real_width,
+        const float &target_height,
+        const float &real_height,
         const size_t &room_num,
-        const float &room_width,
-        const float &room_height);
+        const std::vector<std::string> &room_name_vec);
+
+    bool setWallRoomContainer(
+        const size_t &wall_roomcontainer_data_idx,
+        const size_t &wall_id,
+        const NodeType &wall_type,
+        const size_t &on_wall_boundary_idx,
+        const float &on_wall_boundary_start_position,
+        const float &target_width,
+        const float &real_width,
+        const float &target_height,
+        const float &real_height,
+        const size_t &room_num,
+        const std::vector<std::string> &room_name_vec);
+
+    bool setFreeRoomContainer(
+        const size_t &team_x_direction_person_num,
+        const size_t &team_y_direction_person_num,
+        const float &team_dist,
+        const float &person_edge);
+
+public:
+    bool outputInfo(
+        const size_t &info_level);
+
+    WorldData world_data;
+    std::vector<WallData> wall_data_vec;
+    std::vector<WallRoomContainerData> wall_roomcontainer_data_vec;
+    FreeRoomContainerData free_roomcontainer_data;
 };
 
 #endif //WORLD_DESCRIPTOR_H

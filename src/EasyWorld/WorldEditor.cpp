eee336392d77473da9216cb28cf29e6ba73623a3
chLi <565353780@qq.com>
2021-11-09 15:59:07 +0800

    now all rooms can move to any wall

--- src/EasyWorld/WorldEditor.cpp
+++ src/EasyWorld/WorldEditor.cpp
@@ -28,139 +28,105 @@ bool WorldEditor::setWallRoomContainerPosition(
         return false;
     }
 
-    EasyNode* wall_roomcontainer_node = world_controller.findNode(
-        wall_roomcontainer_id, NodeType::RoomContainer);
-
-    if(wall_roomcontainer_node == nullptr)
-    {
-        std::cout << "WorldEditor::setWallRoomContainerPosition : " << std::endl <<
-          "Input :\n" <<
-          "\twall_roomcontainer_id = " << wall_roomcontainer_id << std::endl <<
-          "\tnew_position = [" << new_position_x << "," <<
-          new_position_y << "]" << std::endl <<
-          "\tmouse_pos_x_direction_delta = " << mouse_pos_x_direction_delta << std::endl <<
-          "this wall roomcontainer not found!" << std::endl;
-
-        return false;
-    }
-
-    EasyNode* wall_roomcontainer_parent_node = wall_roomcontainer_node->getParent();
-    if(wall_roomcontainer_parent_node == nullptr)
-    {
-        std::cout << "WorldEditor::setWallRoomContainerPosition : " << std::endl <<
-          "Input :\n" <<
-          "\twall_roomcontainer_id = " << wall_roomcontainer_id << std::endl <<
-          "\tnew_position = [" << new_position_x << "," <<
-          new_position_y << "]" << std::endl <<
-          "\tmouse_pos_x_direction_delta = " << mouse_pos_x_direction_delta << std::endl <<
-          "this wall roomcontainer's parent not exist!" << std::endl;
-
-        return false;
-    }
-
-    EasyNode* wall_node = wall_roomcontainer_parent_node->getParent();
-    if(wall_node == nullptr)
-    {
-        std::cout << "WorldEditor::setWallRoomContainerPosition : " << std::endl <<
-          "Input :\n" <<
-          "\twall_roomcontainer_id = " << wall_roomcontainer_id << std::endl <<
-          "\tnew_position = [" << new_position_x << "," <<
-          new_position_y << "]" << std::endl <<
-          "\tmouse_pos_x_direction_delta = " << mouse_pos_x_direction_delta << std::endl <<
-          "this wall roomcontainer's parent wall node not exist!" << std::endl;
-
-        return false;
-    }
-
     EasyPoint2D new_position_in_world;
     new_position_in_world.setPosition(new_position_x, new_position_y);
 
-    EasyPoint2D new_position_in_wall_node;
-    if(!wall_node->getPointInNode(
-          new_position_in_world,
-          new_position_in_wall_node))
-    {
-        std::cout << "WorldEditor::setWallRoomContainerPosition : " << std::endl <<
-          "Input :\n" <<
-          "\twall_roomcontainer_id = " << wall_roomcontainer_id << std::endl <<
-          "\tnew_position = [" << new_position_x << "," <<
-          new_position_y << "]" << std::endl <<
-          "\tmouse_pos_x_direction_delta = " << mouse_pos_x_direction_delta << std::endl <<
-          "getPointInNode in parent wall node failed!" << std::endl;
+    float min_dist2_to_wall_boundary_line = std::numeric_limits<float>::max();
+    size_t min_dist_wall_id;
+    NodeType min_dist_wall_type;
+    size_t min_dist_wall_boundary_idx;
+    EasyPoint2D min_dist_wall_boundary_start_point;
+    EasyPoint2D nearest_point_on_line;
 
-        return false;
+    for(size_t i = 0; i < world_place_generator.boundary_line_manager_.wall_boundary_line_list_vec_.size(); ++i)
+    {
+        const WallBoundaryLineList &current_wall_boundary_line_list =
+          world_place_generator.boundary_line_manager_.wall_boundary_line_list_vec_[i];
+
+        const size_t &current_wall_id = current_wall_boundary_line_list.wall_id_;
+        const NodeType &current_wall_type = current_wall_boundary_line_list.wall_type_;
+        const EasyPolygon2D &current_wall_boundary_polygon =
+          current_wall_boundary_line_list.wall_boundary_polygon_;
+
+        if(current_wall_boundary_polygon.point_list.size() == 0)
+        {
+            std::cout << "WorldEditor::setWallRoomContainerPosition : " << std::endl <<
+              "Input :\n" <<
+              "\twall_roomcontainer_id = " << wall_roomcontainer_id << std::endl <<
+              "\tnew_position = [" << new_position_x << "," <<
+              new_position_y << "]" << std::endl <<
+              "\tmouse_pos_x_direction_delta = " << mouse_pos_x_direction_delta << std::endl <<
+              "current wall boundary polygon is empty!" << std::endl;
+
+            return false;
+        }
+
+        EasyNode* current_wall_node = world_controller.findNode(current_wall_id, current_wall_type);
+        if(current_wall_node == nullptr)
+        {
+            std::cout << "WorldEditor::setWallRoomContainerPosition : " << std::endl <<
+              "Input :\n" <<
+              "\twall_roomcontainer_id = " << wall_roomcontainer_id << std::endl <<
+              "\tnew_position = [" << new_position_x << "," <<
+              new_position_y << "]" << std::endl <<
+              "\tmouse_pos_x_direction_delta = " << mouse_pos_x_direction_delta << std::endl <<
+              "findNode for current wall node failed!" << std::endl;
+
+            return false;
+        }
+
+        EasyPoint2D new_position_in_current_wall_node;
+        if(!current_wall_node->getPointInNode(
+              new_position_in_world,
+              new_position_in_current_wall_node))
+        {
+            std::cout << "WorldEditor::setWallRoomContainerPosition : " << std::endl <<
+              "Input :\n" <<
+              "\twall_roomcontainer_id = " << wall_roomcontainer_id << std::endl <<
+              "\tnew_position = [" << new_position_x << "," <<
+              new_position_y << "]" << std::endl <<
+              "\tmouse_pos_x_direction_delta = " << mouse_pos_x_direction_delta << std::endl <<
+              "getPointInNode in wall node failed!" << std::endl;
+
+            return false;
+        }
+
+        for(size_t j = 0; j < current_wall_boundary_polygon.point_list.size(); ++j)
+        {
+            const EasyPoint2D &current_point = current_wall_boundary_polygon.point_list[j];
+            const EasyPoint2D &next_point = current_wall_boundary_polygon.point_list[
+            (j + 1) % current_wall_boundary_polygon.point_list.size()];
+
+            EasyLine2D current_wall_boundary_line;
+            current_wall_boundary_line.setPosition(current_point, next_point);
+
+            const EasyPoint2D current_nearest_point_on_line = EasyComputation::getNearestPointOnLine(
+                current_wall_boundary_line, new_position_in_current_wall_node);
+
+            const float current_dist2_to_wall_boundary_line =
+              EasyComputation::pointDist2(new_position_in_current_wall_node, current_nearest_point_on_line);
+
+            if(current_dist2_to_wall_boundary_line < min_dist2_to_wall_boundary_line)
+            {
+                min_dist2_to_wall_boundary_line = current_dist2_to_wall_boundary_line;
+                min_dist_wall_id = current_wall_id;
+                min_dist_wall_type = current_wall_type;
+                min_dist_wall_boundary_idx = j;
+                min_dist_wall_boundary_start_point = current_point;
+                nearest_point_on_line = current_nearest_point_on_line;
+            }
+        }
     }
 
     WallRoomContainerData &wall_roomcontainer_data =
       world_descriptor_.wall_roomcontainer_data_vec[wall_roomcontainer_id];
 
-    const size_t &wall_id = wall_roomcontainer_data.wall_id;
-    const NodeType &wall_type = wall_roomcontainer_data.wall_type;
-
-    size_t wall_boundary_line_list_idx;
-    if(!world_place_generator.boundary_line_manager_.getWallBoundaryLineListIdx(
-          wall_id, wall_type, wall_boundary_line_list_idx))
-    {
-        std::cout << "WorldEditor::setWallRoomContainerPosition : " << std::endl <<
-          "Input :\n" <<
-          "\twall_roomcontainer_id = " << wall_roomcontainer_id << std::endl <<
-          "\tnew_position = [" << new_position_x << "," <<
-          new_position_y << "]" << std::endl <<
-          "\tmouse_pos_x_direction_delta = " << mouse_pos_x_direction_delta << std::endl <<
-          "getWallBoundaryLineListIdx failed!" << std::endl;
-
-        return false;
-    }
-
-    const WallBoundaryLineList &wall_boundary_line_list =
-      world_place_generator.boundary_line_manager_.wall_boundary_line_list_vec_[wall_boundary_line_list_idx];
-
-    const EasyPolygon2D &wall_boundary_polygon = wall_boundary_line_list.wall_boundary_polygon_;
-
-    if(wall_boundary_polygon.point_list.size() == 0)
-    {
-        std::cout << "WorldEditor::setWallRoomContainerPosition : " << std::endl <<
-          "Input :\n" <<
-          "\twall_roomcontainer_id = " << wall_roomcontainer_id << std::endl <<
-          "\tnew_position = [" << new_position_x << "," <<
-          new_position_y << "]" << std::endl <<
-          "\tmouse_pos_x_direction_delta = " << mouse_pos_x_direction_delta << std::endl <<
-          "wall_boundary_polygon is empty!" << std::endl;
-
-        return false;
-    }
-
-    float min_dist2_to_wall_boundary_line = std::numeric_limits<float>::max();
-    size_t min_dist_wall_boundary_idx;
-    EasyPoint2D nearest_point_on_line;
-
-    for(size_t i = 0; i < wall_boundary_polygon.point_list.size(); ++i)
-    {
-        const EasyPoint2D &current_point = wall_boundary_polygon.point_list[i];
-        const EasyPoint2D &next_point = wall_boundary_polygon.point_list[
-        (i + 1) % wall_boundary_polygon.point_list.size()];
-
-        EasyLine2D current_wall_boundary_line;
-        current_wall_boundary_line.setPosition(current_point, next_point);
-
-        const EasyPoint2D current_nearest_point_on_line = EasyComputation::getNearestPointOnLine(
-            current_wall_boundary_line, new_position_in_wall_node);
-
-        const float current_dist2_to_wall_boundary_line =
-          EasyComputation::pointDist2(new_position_in_wall_node, current_nearest_point_on_line);
-
-        if(current_dist2_to_wall_boundary_line < min_dist2_to_wall_boundary_line)
-        {
-            min_dist2_to_wall_boundary_line = current_dist2_to_wall_boundary_line;
-            min_dist_wall_boundary_idx = i;
-            nearest_point_on_line = current_nearest_point_on_line;
-        }
-    }
-
+    wall_roomcontainer_data.wall_id = min_dist_wall_id;
+    wall_roomcontainer_data.wall_type = min_dist_wall_type;
     wall_roomcontainer_data.on_wall_boundary_idx = min_dist_wall_boundary_idx;
 
     const float nearest_point_on_line_dist_to_boundary_line_start_point =
-      EasyComputation::pointDist(nearest_point_on_line, wall_boundary_polygon.point_list[min_dist_wall_boundary_idx]);
+      EasyComputation::pointDist(nearest_point_on_line, min_dist_wall_boundary_start_point);
 
     wall_roomcontainer_data.on_wall_boundary_start_position =
       nearest_point_on_line_dist_to_boundary_line_start_point - mouse_pos_x_direction_delta;

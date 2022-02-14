3ed83b6f78acff5cf61d1ebc4830344787e35dfd
565353780 <565353780@qq.com>
2022-02-14 12:58:11 +0800

    remove python scripts

--- scripts/World/Data/Point.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-class Point(object):
-    def __init__(self):
-        self.x = None
-        self.y = None
-        return
-
--- scripts/World/Data/Polygon.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-class Polygon(object):
-    def __init__(self):
-        self.point_list = []
-        return
-
